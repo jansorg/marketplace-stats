29aa64f4dfdd5a1245ceb474a3cb666c4afad722
jansorg <github@ja-dev.eu>
2021-05-06 19:25:08 +0200

    Print report in landscape to display all content of tables, update random report.

--- cmds/update-random-report.bash
+++ cmds/update-random-report.bash
@@ -10,4 +10,4 @@ cd "$DIR/.."
 go build -o random-report ./cmds/random-report
 ./random-report > ./random-report.html
 puppeteer --wait-until "networkidle2" print ./random-report.html ./random-report.pdf
-pdftoppm -jpeg -r 160 -singlefile ./random-report.pdf random-report
+pdftoppm -jpeg -jpegopt "quality=80,optimize=y" -r 120 -singlefile ./random-report.pdf random-report
